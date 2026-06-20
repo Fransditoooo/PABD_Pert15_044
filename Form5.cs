267a70b9902169de49017159f2e5178ca263bafd
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2026-06-20 19:46:53 +0700

    menambahkan logika backend dasar dan connection string pada Form4

---
+++ Form5.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class Form5 : Form
+    {
+        DAL dbLogic = new DAL();
+
+        // Menggunakan string koneksi terpadu yang sama dengan Form lainnya
+        static string connectionString = "Data Source=FRANSSS\\FRANSDITO;Initial Catalog=DBAkademikADO;Integrated Security=True";
+
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+
+        ListMahasiswa listMahasiswa = new ListMahasiswa();
+
+        string prodi { get; set; }
+        DateTime tglmasuk { get; set; }
+
+        public Form5(string prodi, DateTime TglMasuk)
+        {
+            InitializeComponent();
+
+            prodi = prodi;
+            tglmasuk = TglMasuk;
+
+            try
+            {
+                DataTable dtMahasiswa = dbLogic.getDataRekap(prodi, tglmasuk);
+
+                listMahasiswa.SetDataSource(dtMahasiswa);
+                crystalReportViewer1.ReportSource = listMahasiswa;
+                crystalReportViewer1.Refresh();
+            }
+            catch (Exception ex)
+            {
+                //simpanLog(ex.Message);
+                MessageBox.Show("Gagal load data: " + ex.Message);
+            }
+        }
+    }
+}
